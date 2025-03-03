a743af826f73c6b6ffab01eb27ef78d841e5f6ba
Randy Ubillos <randy@mac.com>
2025-03-03 15:08:00 -0800

    Script to merge hash lists

---
+++ make-both-hash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+input1="Processed/excludes-hash.txt"
+input2="Others/Rick/FB/excludes-hash.txt"
+output="Others/Both/fb/excludes-hash.txt"
+
+{
+  cat "$input1"
+  echo -e "\r"
+  cat "$input2"
+  echo -e "\r"
+} > "$output"
